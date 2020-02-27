aa51e8d2038d239262e6056df49697b0ea069253
Muhammad Faiz Alviansyah <faizalvian97@gmail.com>
2020-02-27 18:34:10 +0700

    Upload Shayna Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroSectionCarousel/>
+    <BannerSection/>
+    <InstaGalleries/>
+    <PartnerLogo/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSectionCarousel from '@/components/HeroSectionCarousel.vue'
+import BannerSection from '@/components/BannerSection.vue'
+import InstaGalleries from '@/components/InstaGalleries.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+ 
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroSectionCarousel,
+    BannerSection ,
+    InstaGalleries,
+    PartnerLogo,
+    FooterShayna
+  }
+}
+</script>
